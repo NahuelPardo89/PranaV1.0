0a8978eb84069a8bfc808f74f9e680c0b57c7701
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-06-29 12:46:23 -0300

    ViewSet users funcional

--- apiRest/apps/users/api/serializers.py
+++ apiRest/apps/users/api/serializers.py
@@ -6,8 +6,44 @@ from apps.users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model=User
-        fields='__all__'
+        model = User
+        fields = '__all__'
+    
+    def create(self,validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('dni', 'email', 'name', 'last_name')
+
+class PasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=128, min_length=6, write_only=True)
+    password2 = serializers.CharField(max_length=128, min_length=6, write_only=True)
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError(
+                {'password':'Debe ingresar ambas contraseñas iguales'}
+            )
+        return data
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+    def to_representation(self, instance):
+        return {
+            'id': instance['id'],
+            'dni': instance['dni'],
+            'name': instance['name'],
+            'last_name': instance['last_name'],
+            'email': instance['email'],
+            'phone': instance['phone'],
+        }
 
 class LoginSerializer(serializers.Serializer):
     dni = serializers.IntegerField()
--- apiRest/apps/users/api/views.py
+++ apiRest/apps/users/api/views.py
@@ -1,21 +1,94 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets,permissions,status,generics
-
-
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.users.api.serializers import UserSerializer , LoginSerializer
 from apps.users.models import User
+from apps.users.api.serializers import (
+    UserSerializer, UserListSerializer, UpdateUserSerializer,
+    PasswordSerializer
+)
 
-class UserAPIView(APIView):
-    permission_classes = [permissions.IsAuthenticated, ]
+class UserViewSet(viewsets.ViewSet):
+    model = User
+    serializer_class = UserSerializer
+    list_serializer_class = UserListSerializer
+    queryset = None
 
-    def get(self, request):
-        users=User.objects.all()
-        users_serializer=UserSerializer(users, many=True)
-        return Response(users_serializer.data)
+    def get_object(self, pk):
+        return get_object_or_404(self.model, pk=pk)
+
+    def get_queryset(self):
+        if self.queryset is None:
+            self.queryset = self.model.objects\
+                            .filter(is_active=True)\
+                            .values('id', 'dni', 'email', 'name','last_name','phone')
+        return self.queryset
+
+    @action(detail=True, methods=['post'])
+    def set_password(self, request, pk=None):
+        user = self.get_object(pk)
+        password_serializer = PasswordSerializer(data=request.data)
+        if password_serializer.is_valid():
+            user.set_password(password_serializer.validated_data['password'])
+            user.save()
+            return Response({
+                'message': 'Contraseña actualizada correctamente'
+            })
+        return Response({
+            'message': 'Hay errores en la información enviada',
+            'errors': password_serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request):
+        users = self.get_queryset()
+        users_serializer = self.list_serializer_class(users, many=True)
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        user_serializer = self.serializer_class(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({
+                'message': 'Usuario registrado correctamente.'
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'message': 'Hay errores en el registro',
+            'errors': user_serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        user = self.get_object(pk)
+        user_serializer = self.serializer_class(user)
+        return Response(user_serializer.data)
+    
+    def update(self, request, pk=None):
+        user = self.get_object(pk)
+        user_serializer = UpdateUserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({
+                'message': 'Usuario actualizado correctamente'
+            }, status=status.HTTP_200_OK)
+        return Response({
+            'message': 'Hay errores en la actualización',
+            'errors': user_serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user_destroy = self.model.objects.filter(id=pk).update(is_active=False)
+        if user_destroy == 1:
+            return Response({
+                'message': 'Usuario eliminado correctamente'
+            })
+        return Response({
+            'message': 'No existe el usuario que desea eliminar'
+        }, status=status.HTTP_404_NOT_FOUND)
+
 
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginSerializer
@@ -35,6 +108,7 @@ class LoginAPI(generics.GenericAPIView):
             return Response({"message":"Usuario o Contraseña incorrecto"}, status=status.HTTP_400_BAD_REQUEST)
 
 class LogoutAPI(generics.GenericAPIView):
+    
     permission_classes = [permissions.IsAuthenticated, ]
 
     def post(self, request):

99ef1fc4dc666652b5e9db2a0ce7aa3e7b9513bb
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-06-29 12:46:42 -0300

    router UserViewset

--- apiRest/apps/users/api/urls.py
+++ apiRest/apps/users/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from apps.users.api.views import UserAPIView,LoginAPI,LogoutAPI
+from rest_framework.routers import DefaultRouter
+from apps.users.api.views import LoginAPI,LogoutAPI,UserViewSet
+
+router = DefaultRouter()
+
+router.register('users', UserViewSet, basename="users")
 
 urlpatterns=[
-    path('usuario/',UserAPIView.as_view(), name='usuario_api'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', LogoutAPI.as_view(), name='logout'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+]+router.urls

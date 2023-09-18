b4f22c310b0ca75abff3d626d488f6e91bed83a5
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-09-18 10:32:51 -0300

    appoiment admin components

--- Frontend/prana/src/app/Modules/appointments/appointments-routing.module.ts
+++ Frontend/prana/src/app/Modules/appointments/appointments-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Components
-import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
-import { AppointmentDetailComponent } from './components/appointment-detail/appointment-detail.component';
-import { AppointmentCreateComponent } from './components/appointment-create/appointment-create.component';
-import { AppointmentUpdateComponent } from './components/appointment-update/appointment-update.component';
-import { AppointmentDeleteComponent } from './components/appointment-delete/appointment-delete.component';
+import { AppointmentListComponent } from './components/admin/appointment-list/appointment-list.component';
+import { AppointmentDetailComponent } from './components/admin/appointment-detail/appointment-detail.component';
+import { AppointmentCreateComponent } from './components/admin/appointment-create/appointment-create.component';
+import { AppointmentUpdateComponent } from './components/admin/appointment-update/appointment-update.component';
+import { AppointmentDeleteComponent } from './components/admin/appointment-delete/appointment-delete.component';
 
 const routes: Routes = [
   {
--- Frontend/prana/src/app/Modules/appointments/appointments.module.ts
+++ Frontend/prana/src/app/Modules/appointments/appointments.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppointmentsRoutingModule } from './appointments-routing.module';
-import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
-import { AppointmentDetailComponent } from './components/appointment-detail/appointment-detail.component';
-import { AppointmentCreateComponent } from './components/appointment-create/appointment-create.component';
-import { AppointmentUpdateComponent } from './components/appointment-update/appointment-update.component';
-import { AppointmentDeleteComponent } from './components/appointment-delete/appointment-delete.component';
+import { AppointmentListComponent } from './components/admin/appointment-list/appointment-list.component';
+import { AppointmentDetailComponent } from './components/admin/appointment-detail/appointment-detail.component';
+import { AppointmentCreateComponent } from './components/admin/appointment-create/appointment-create.component';
+import { AppointmentUpdateComponent } from './components/admin/appointment-update/appointment-update.component';
+import { AppointmentDeleteComponent } from './components/admin/appointment-delete/appointment-delete.component';
 import { AppointmentService } from 'src/app/Services/appointments/appointment.service';
 
 

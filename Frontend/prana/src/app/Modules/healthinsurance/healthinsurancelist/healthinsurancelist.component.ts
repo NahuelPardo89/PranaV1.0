ff7c967ba6bf29dcb426813e8e6c58a9021926f3
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-09-19 12:02:25 -0300

    agregue botones

--- Frontend/prana/src/app/Modules/healthinsurance/healthinsurancelist/healthinsurancelist.component.ts
+++ Frontend/prana/src/app/Modules/healthinsurance/healthinsurancelist/healthinsurancelist.component.ts
@@ -25,4 +25,15 @@ export class HealthinsurancelistComponent {
       });
     }
   }
+  onEdit(Id: number): void {
+    // Aquí puedes agregar la lógica para manejar la edición de la especialidad médica.
+    console.log(`Editing speciality with ID: ${Id}`);
+}
+
+  onDelete(Id: number): void {
+    // Aquí puedes agregar la lógica para manejar la eliminación de la especialidad médica.
+    if (confirm('¿Estas seguro que deseas elimninar esta Obra Social?')) {
+    console.log(`Deleting speciality with ID: ${Id}`);
+    }
+}
 }

ef5d490daaebab3608be679575db86f1e0385572
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-09-19 12:02:03 -0300

    agregue botones

--- Frontend/prana/src/app/Modules/doctor-profile/components/list-doctor-profile/list-doctor-profile.component.ts
+++ Frontend/prana/src/app/Modules/doctor-profile/components/list-doctor-profile/list-doctor-profile.component.ts
@@ -23,5 +23,16 @@ export class ListDoctorProfileComponent {
       console.error('Error loading doctor profiles', error);
     });
   }
+  onEdit(Id: number): void {
+    // Aquí puedes agregar la lógica para manejar la edición de la especialidad médica.
+    console.log(`Editing speciality with ID: ${Id}`);
+}
+
+  onDelete(Id: number): void {
+    // Aquí puedes agregar la lógica para manejar la eliminación de la especialidad médica.
+    if (confirm('¿Estas seguro que deseas elimninar este doctor?')) {
+    console.log(`Deleting speciality with ID: ${Id}`);
+    }
+}
 }
 

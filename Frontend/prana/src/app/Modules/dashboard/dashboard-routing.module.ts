78e6c7c9e03caf55f66795c0fddfc4a36e9465bd
Catriel <cartup90@gmail.com>
2023-09-20 00:00:44 -0300

    Finalizando routing Dashboard

--- Frontend/prana/src/app/Modules/dashboard/dashboard-routing.module.ts
+++ Frontend/prana/src/app/Modules/dashboard/dashboard-routing.module.ts
@@ -20,7 +20,10 @@ const routes: Routes = [
       { path: 'insurance', loadChildren: () => import('src/app/Modules/healthinsurance/healthinsurance.module').then(m => m.HealthinsuranceModule) },
       { path: 'doctors', loadChildren: () => import('src/app/Modules/doctor-profile/doctor-profile.module').then(m => m. DoctorProfileModule) },
       { path: 'appointments', loadChildren: () => import('src/app/Modules/appointments/appointments.module').then(m => m.AppointmentsModule) },
+      { path: 'appointments_doctor', loadChildren: () => import('src/app/Modules/appointments/appointments.module').then(m => m.AppointmentsModule) },
       { path: 'speciality', loadChildren: () => import('src/app/Modules/medicalspeciality/medicalspeciality.module').then(m => m. MedicalspecialityModule) },
+      { path: 'speciality/branch', loadChildren: () => import('src/app/Modules/speciality-branch/speciality-branch.module').then(m => m. SpecialityBranchModule) },
+      { path: 'patient', component: PatientComponent},
       { path: 'speciality/branch', loadChildren: () => import('src/app/Modules/speciality-branch/speciality-branch.module').then(m => m. SpecialityBranchModule) },
   
     ]

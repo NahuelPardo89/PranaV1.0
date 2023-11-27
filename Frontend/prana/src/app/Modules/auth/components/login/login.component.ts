97b7b51b041e2361029c562391e7b5b2e76d7478
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-11-27 09:18:04 -0300

    solucion errores merge

--- Frontend/prana/src/app/Modules/auth/components/login/login.component.ts
+++ Frontend/prana/src/app/Modules/auth/components/login/login.component.ts
@@ -37,15 +37,7 @@ export class LoginComponent implements OnInit , OnDestroy{
 
 
   
-      this.authService.login(user).subscribe(
-        () => {
-          // Una vez que el login es exitoso, navegamos al dashboard.
-          this.router.navigate(['Dashboard/']);
-        },
-        error => {
-          this.errorMessage = 'DNI o Contraseña incorrectos. Por favor, revisa los datos e inténtalo de nuevo.';
-        }
-      );
+      this.authService.login(user)
 
     }
   }

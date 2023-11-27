dc301f04cb5d3408529447d75e9c0b152c56e91c
Catriel <cartup90@gmail.com>
2023-11-27 00:00:34 -0300

    navegación de botones home

--- Frontend/prana/src/app/Modules/home/components/staff/staff.component.ts
+++ Frontend/prana/src/app/Modules/home/components/staff/staff.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StaffService } from 'src/app/Services/staff.service';
-
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-staff',
@@ -11,16 +11,40 @@ export class StaffComponent implements OnInit {
   
   staff: any[] = [];
 
-  constructor(private staffService: StaffService) { }
+  constructor(private staffService: StaffService, private viewportScroller: ViewportScroller) { }
+
+  scrollToSection() {
+    const targetElement = document.getElementById('encontrarnos');
+  
+    if (targetElement) {
+      const offset = window.innerHeight * 0.3;
+      const targetPosition = targetElement.offsetTop - offset;
+  
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+  scrollToStaff() {
+    const targetElement = document.getElementById('staff');
+  
+    if (targetElement) {
+      const offset = window.innerHeight * 0.3;
+      const targetPosition = targetElement.offsetTop - offset;
+  
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+
 
   ngOnInit(): void {
     this.staffService.getStaff().subscribe(data => {
       this.staff = data.profesionales.map((pro: any) => ({ ...pro, showDescription: false }));
-
     });
   }
-
-  // toggleDescription(index: number): void {
-  //   this.staff[index].showDescription = !this.staff[index].showDescription;
-  // }
 }

163387b482908fe1921c354d4d3f17d98103897a
Catriel <cartup90@gmail.com>
2023-11-26 23:01:48 -0300

    resolviendo fix

--- Frontend/prana/src/app/Modules/doctor-profile/doctor-profile.module.ts
+++ Frontend/prana/src/app/Modules/doctor-profile/doctor-profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { DoctorProfileRoutingModule } from './doctor-profile-routing.module';
 import { DoctorprofileService } from 'src/app/Services/Profile/doctorprofile/doctorprofile.service';
 import { ListDoctorProfileComponent } from './components/list-doctor-profile/list-doctor-profile.component';
@@ -18,8 +18,8 @@ import { MeDoctorProfileComponent } from './components/me-doctor-profile/me-doct
   imports: [
     CommonModule,
     DoctorProfileRoutingModule,
-    ReactiveFormsModule,,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    
   ],
   providers: [DoctorprofileService]
 })

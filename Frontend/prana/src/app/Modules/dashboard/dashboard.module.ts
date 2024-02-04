8a3c964bca1c9a9ed8915ecae7b197f117783c75
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-02-04 12:16:42 -0300

    eliminado componetente innesesario

--- Frontend/prana/src/app/Modules/dashboard/dashboard.module.ts
+++ Frontend/prana/src/app/Modules/dashboard/dashboard.module.ts
@@ -6,10 +6,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardNavComponent } from './dashboard-nav/dashboard-nav.component';
 import { DashboardBodyComponent } from './dashboard-body/dashboard-body.component';
-import { PatientComponent } from './patient/patient.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ListarPacientesComponent } from './patient/listar-pacientes/listar-pacientes.component';
+
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -23,8 +23,8 @@ import { MatSelectModule } from '@angular/material/select';
     DashboardComponent,
     DashboardNavComponent,
     DashboardBodyComponent,
-    PatientComponent,
-    ListarPacientesComponent,
+  
+    
     
   ],
   imports: [

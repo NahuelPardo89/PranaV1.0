c59c82e9908abae57afc225a6f562505897dced3
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-02-26 18:44:33 -0300

    static rooy modificada

--- apiRest/core/settings/base.py
+++ apiRest/core/settings/base.py
@@ -150,8 +150,15 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static",
+STATIC_URL = '/static/'
+
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

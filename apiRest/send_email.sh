f79fdf44998f09c640ba63d95524a8ed192941ed
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-05-20 11:13:10 -0300

    ultimo mambo de log

--- apiRest/send_email.sh
+++ apiRest/send_email.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
 # Activar el entorno virtual
 source /home/prueba/htdocs/PranaV1.0/apiRest/env/bin/activate
 
 # Cambiar al directorio del proyecto
 cd /home/prueba/htdocs/PranaV1.0/apiRest
 
-# Ejecutar el comando de Django
-python manage.py send_email_remainder
+# Crear el archivo de log con fecha y hora
+LOG_FILE="/home/prueba/htdocs/PranaV1.0/apiRest/logs/send_email_$(date +'%Y-%m-%d_%H-%M-%S').log"
+
+# Ejecutar el comando de Django y redirigir la salida a un archivo de log
+python manage.py send_email_remainder >> "$LOG_FILE" 2>&1
 
 # Desactivar el entorno virtual (opcional, ya que el script terminará)
 deactivate

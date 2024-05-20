46f9364aa779a0631766b7bb21badc0dc0b9b036
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-05-19 22:13:33 -0300

    ASE

---
+++ apiRest/send_email.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Activar el entorno virtual
+source /home/prueba/htdocs/PranaV1.0/env/bin/activate
+
+# Cambiar al directorio del proyecto
+cd /home/prueba/htdocs/PranaV1.0/apiRest
+
+# Ejecutar el comando de Django
+python manage.py send_email_remainder
+
+# Desactivar el entorno virtual (opcional, ya que el script terminará)
+deactivate

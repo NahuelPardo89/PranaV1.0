d9432a3bba5eb10985da5872e8502bbb7056fca4
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-05-23 15:23:58 -0300

    send email del servidor

--- apiRest/send_email_remainder.sh
+++ apiRest/send_email_remainder.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Activar el entorno virtual
-source /home/prueba/htdocs/PranaV1.0/apiRest/env/bin/activate
+source /home/apirestprana/htdocs/api.centroterapeuticoprana.com/venv/bin/activate
 
 # Cambiar al directorio del proyecto
-cd /home/prueba/htdocs/PranaV1.0/apiRest
+cd /home/apirestprana/htdocs/api.centroterapeuticoprana.com/PranaV1.0/apiRest
 
 # Crear el archivo de log con fecha y hora
-LOG_FILE="/home/prueba/htdocs/PranaV1.0/apiRest/logs/send_email_$(date +'%Y-%m-%d_%H-%M-%S').log"
+LOG_FILE="/home/apirestprana/htdocs/api.centroterapeuticoprana.com/PranaV1.0/apiRest/logs/send_email_$(date +'%Y-%m-%d_%H-%M-%S').log"
 
 # Ejecutar el comando de Django y redirigir la salida a un archivo de log
 python manage.py send_email_remainder >> "$LOG_FILE" 2>&1

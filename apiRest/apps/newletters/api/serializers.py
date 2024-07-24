e338ade24feead027cfd82496b64a24048169610
Alvaro Olguin <alvaroarmendariz11@gmail.com>
2024-07-24 12:24:20 -0300

    Newsletter suscribe and send views and template for email

--- apiRest/apps/newletters/api/serializers.py
+++ apiRest/apps/newletters/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from apps.newletters.models import Newletter
 
+
 class NewletterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Newletter
--- apiRest/apps/newletters/models.py
+++ apiRest/apps/newletters/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Newletter(models.Model):
-    email = models.EmailField('Correo Electrónico', max_length=255, unique=True,)
+    email = models.EmailField('Correo Electrónico',
+                              max_length=255, unique=True,)
 
     def __str__(self):
         return self.email

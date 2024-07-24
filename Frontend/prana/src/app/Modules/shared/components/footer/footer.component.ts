7016ff55195780d8caf867a9a9e4b21caaeae933
NahuelPardo89 <106750741+NahuelPardo89@users.noreply.github.com>
2024-07-24 19:00:33 -0300

    cambio de modulo a footer

--- Frontend/prana/src/app/Modules/shared/components/footer/footer.component.ts
+++ Frontend/prana/src/app/Modules/shared/components/footer/footer.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  newsletterForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.newsletterForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  get email() {
+    return this.newsletterForm.get('email');
+  }
+
+  onSubmit() {
+    if (this.newsletterForm.valid) {
+      console.log('Email registrado:', this.newsletterForm.value.email);
+    }
+  }
 }

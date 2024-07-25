5d499e20f4e9d35e7ba450c8f6d169f1bcb38c87
NahuelPardo89 <nahuel.pardo74@gmail.com>
2024-07-25 10:43:35 -0300

    footer newletter done

--- Frontend/prana/src/app/Modules/shared/components/footer/footer.component.ts
+++ Frontend/prana/src/app/Modules/shared/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NewletterService } from 'src/app/Services/newletter/newletter.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,8 +10,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FooterComponent implements OnInit {
   newsletterForm: FormGroup;
+  successMessage: string = '';
+  errorMessage: string = '';
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private newsletterService: NewletterService) {
     this.newsletterForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     });
@@ -23,7 +27,23 @@ export class FooterComponent implements OnInit {
 
   onSubmit() {
     if (this.newsletterForm.valid) {
-      console.log('Email registrado:', this.newsletterForm.value.email);
+      this.newsletterService.subscribe(this.newsletterForm.value.email).subscribe(
+        response => {
+          console.log('Email registrado:', response);
+          this.successMessage = 'Subscripción realizada con éxito';
+          this.errorMessage = '';
+          this.newsletterForm.reset();
+        },
+        (error: HttpErrorResponse) => {
+          console.error('Error al registrar el email:', error);
+          if (error.status === 400 && error.error?.email?.[0] === 'Ya existe un/a newletter con este/a Correo Electrónico.') {
+            this.errorMessage = 'El Email ingresado ya se encuentra registrado';
+          } else {
+            this.errorMessage = 'Ocurrió un error al registrar el email. Inténtalo de nuevo más tarde.';
+          }
+          this.successMessage = '';
+        }
+      );
     }
   }
 }

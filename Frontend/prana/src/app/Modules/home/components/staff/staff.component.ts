d1fb2b33e5824880e07aa49cd049f6e86537bf7f
Catriel <cartup90@gmail.com>
2024-07-29 12:28:18 -0300

    depuración

--- Frontend/prana/src/app/Modules/home/components/staff/staff.component.ts
+++ Frontend/prana/src/app/Modules/home/components/staff/staff.component.ts
@@ -5,17 +5,10 @@ import { PatientService } from 'src/app/Services/Profile/patient/patient.service
 import { MatTableDataSource } from '@angular/material/table';
 import { Patient } from 'src/app/Models/Profile/patient.interface';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
 import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
+
+// Register Swiper custom elements
 register();
-
-
-
-
-
-
-
 
 @Component({
   selector: 'app-staff',
@@ -23,109 +16,81 @@ register();
   styleUrls: ['./staff.component.css']
 })
 export class StaffComponent implements OnInit {
-
   webInstragram: string = '';
-  
   showObjectives = false;
   dataSource!: MatTableDataSource<Patient>;
-  
   staff: any[] = [];
-
-  
-
   sliderImages: string[] = [
-    "../../../../../assets/img/foto1.jpeg",
-    "../../../../../assets/img/foto2.jpeg",
-    "../../../../../assets/img/foto5.jpeg",
-    "../../../../../assets/img/foto4.jpeg",
-  ]
+   
+  ];
 
-  
+  constructor(
+    private staffService: StaffService,
+    private router: Router,
+    private patientService: PatientService,
+    private sanitizer: DomSanitizer
+  ) {}
 
-  constructor(private staffService: StaffService, private router : Router, private patientService: PatientService, private sanitizer: DomSanitizer) { }
-
- 
-
-  getIg = () => {
+  getIg() {
     this.patientService.getAllPatients().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
-      const filterValue = 'PRANA';
-      this.dataSource.filter = filterValue.trim();
-      this.webInstragram= this.dataSource.filteredData[0].instagram;
-      
+      this.dataSource.filter = 'PRANA';
+      this.webInstragram = this.dataSource.filteredData[0].instagram;
     });
   }
+
   get instagramUrl(): SafeResourceUrl {
     const unsafeUrl = `https://www.instagram.com/${this.webInstragram}/embed/`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
   }
-  
 
   scrollToSection() {
     const targetElement = document.getElementById('encontrarnos');
-  
     if (targetElement) {
       const offset = window.innerHeight * 0.3;
       const targetPosition = targetElement.offsetTop - offset;
-  
       window.scrollTo({
         top: targetPosition,
         behavior: 'smooth'
       });
     }
   }
+
   scrollToBodyNav() {
     const targetElement = document.getElementById('bodyNav');
-  
     if (targetElement) {
       const offset = window.innerHeight * 0.1;
       const targetPosition = targetElement.offsetTop - offset;
-  
       window.scrollTo({
         top: targetPosition,
         behavior: 'smooth'
       });
     }
   }
-  
+
   navigateToAboutUs() {
     this.router.navigate(['/aboutus']);
-    
   }
 
   navigateToProf() {
     this.router.navigate(['/profesionales']);
-    
   }
+
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
     const element = document.querySelector('.objetivos');
     if (element) {
       const rect = element.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-
-      if (topShown && bottomShown) {
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
         this.showObjectives = true;
       }
     }
   }
 
-
-
   ngOnInit(): void {
-    
     this.staffService.getStaff().subscribe(data => {
       this.staff = data.profesionales.map((pro: any) => ({ ...pro, showDescription: false }));
     });
     this.getIg();
   }
-
-
- 
-
-
-  
 }
-
-
